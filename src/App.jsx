b0fd8d42dcbcf54f714690ac1d5e2a2721dc4fa7
o-b3d <obed@ogtesting.com>
2023-11-14 21:14:28 -0500

    third commit,couldn't figure some things out today

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      task: "Enter To-Do",
+      task: "Enter your To-Do Above",
       done: false,
     },
   ]);
@@ -16,39 +16,66 @@ export default function App() {
   const [newTodos, setNewTodos] = useState("");
 
   const addTodo = (text) => {
-    const newTodoList = [
+    const newTodoListForAddTodo = [
       ...todos,
       { id: todos.length + 1, task: text, done: false },
     ];
-    setTodos(newTodoList);
+    setTodos(newTodoListForAddTodo);
   };
 
   const deleteTodo = (id) => {
-    const newTodoList = todo.filter((item) => item.id != id);
-    setTodos(newTodoList);
+    const newTodoListForDeleteTodo = todos.filter((item) => item.id != id);
+    setTodos(newTodoListForDeleteTodo);
   };
+
+  /* prettier-ignore */
+  const updateTodoText = (id, text) => {setTodos()};
+
+  /* Created preventDefault variable since there is no backend for form*/
+  /* prettier-ignore */
+  const preventDefault = (e) => {e.preventDefault()};
 
   return (
     <>
       <div className="todoListContainer">
         <h3>To-Do List {"-->"}</h3>
         <div className="newTodoContainer">
-          <input
-            value={newTodos}
-            placeholder="Add New Todo Temp"
-            onChange={(event) => setNewTodos(event.target.value)}
-          />
-          <button onClick={() => addTodo(newTodos)} className="addButton">
-            Add
-          </button>
+          {/* Wrapped input and button inside Form tag to make the input field work with the enter key */}
+          <form onSubmit={preventDefault}>
+            <input
+              value={newTodos}
+              placeholder="Enter Text Here"
+              onChange={(event) => setNewTodos(event.target.value)}
+            />
+            <button onClick={() => addTodo(newTodos)} className="addButton">
+              Add
+            </button>
+          </form>
         </div>
 
         <ul className="todoList">
           {todos.map((item) => {
             return (
               <li key={item.id} className="todoListItem">
-                <input type="checkbox" value={item.done} />
-                <span className="todoContent">{item.task}</span>
+                <input
+                  type="checkbox"
+                  value={item.done}
+                  /* Same as below, need to figure out how to update the state using the onChange eventListener to
+                  update using striketext css value inside Input Text classname*/
+                  onChange={() => {}}
+                />
+                {/* Replaced below Span tag with Input tag to make the created To-Do modifiable */}
+                {/* -> I have to figure out a way to update the state of "todos" using the onChange eventListener
+                and with the declared variable updateTodoText I created*/}
+                {/* prettier-ignore */}
+                <input
+                  type="text"
+                  /* Added conditional operator to className to add striketext css value to Input Text if checkbox is checked */
+                  className={`todoContent ${item.done ? 'striketext' : ''}`}
+                  value={item.task}
+                  onChange={(event) => updateTodoText(item.id, event.target.value)
+                  }
+                  />
                 <button
                   onClick={() => deleteTodo(item.id)}
                   className="deleteButton"
